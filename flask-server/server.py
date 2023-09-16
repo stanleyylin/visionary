9a21472c2a6346c5fccad59f893e6f242acbf240
Rachel Chen <85042944+rachelchxn@users.noreply.github.com>
2023-09-16 12:00:16 -0400

    COUNTDOWN!!

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,13 +1,81 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import time
 
 app = Flask(__name__)
+
+CORS(app, origins=["http://localhost:3000"])
 
 @app.route('/members')
 def members():
     return jsonify({"members": ["yas", "yaas", "yaaas"]})
 
-CORS(app)
+# States
+NOT_STARTED = 0
+WORK_RUNNING = 1
+WORK_PAUSED = 2
+REST_RUNNING = 3
+REST_PAUSED = 4
+
+state = NOT_STARTED
+WORK_BLOCK = 11   # 10 minutes in seconds
+REST_BLOCK = 21  # 20 minutes in seconds
+
+end_time = None
+remaining_time_when_paused = None
+
+@app.route('/control', methods=['POST'])
+def control():
+    global state, end_time, remaining_time_when_paused
+    action = request.json.get("action")
+    
+    current_time = time.time()
+    
+    if action == "start":
+        state = WORK_RUNNING
+        end_time = current_time + WORK_BLOCK
+    elif action == "pause":
+        if state in [WORK_RUNNING, REST_RUNNING]:
+            remaining_time_when_paused = end_time - current_time
+            end_time = current_time  # Immediately update to reflect paused state
+            state = WORK_PAUSED if state == WORK_RUNNING else REST_PAUSED
+    elif action == "resume":
+        if state in [WORK_PAUSED, REST_PAUSED]:
+            state = WORK_RUNNING if state == WORK_PAUSED else REST_RUNNING
+            end_time = current_time + remaining_time_when_paused
+
+    return jsonify({"status": "ok"})
+
+@app.route('/time_left')
+def time_left():
+    global state, end_time
+    current_time = time.time()
+
+    if state == NOT_STARTED:
+        return jsonify({"time_left": 0, "state": state})
+    
+    # Handle the paused states
+    if state in [WORK_PAUSED, REST_PAUSED]:
+        return jsonify({"time_left": remaining_time_when_paused, "state": state})
+    
+    time_left = end_time - current_time
+
+    if time_left < 0:
+        if state in [WORK_RUNNING, REST_RUNNING]:  # Only reset if running
+            time_left = 0
+            state = REST_RUNNING if state == WORK_RUNNING else WORK_RUNNING
+            end_time = current_time + (REST_BLOCK if state == REST_RUNNING else WORK_BLOCK)
+
+    return jsonify({"time_left": time_left, "state": state})
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    global state, end_time, remaining_time_when_paused
+    state = NOT_STARTED
+    end_time = None
+    remaining_time_when_paused = None
+    return jsonify({"status": "reset"})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from "react";
+import Countdown from "../../components/countdown";
+import styled from "@emotion/styled";
 
 const Dashboard = () => {
   return (
-    <div>
-      Dashboard
-    </div>
-  )
-}
+    <PageContainer>
+      <div>
+        <Countdown />
+      </div>
+    </PageContainer>
+  );
+};
 
-export default Dashboard
+const PageContainer = styled.div`
+  width: 100%;
+  padding: 40px;
+`;
+
+export default Dashboard;
